ce73391de3d7fd9c2e7d8b106be373122a956b8f
Liu Jun <liuj@ihpc.a-star.edu.sg>
2026-04-27 11:02:44 +0800

    Wire galaxea_lab_agent task to the active robot bundle.
    Same refactor as galaxea_lab_external: env_cfg pulls ArticulationCfg,
    camera cfgs, joint dof-names, and rule_policy_class from
    ACTIVE_ROBOT_BUNDLE; the env class instantiates
    self.cfg.rule_policy_class(...) instead of importing GalaxeaRulePolicy.

--- source/Galaxea_Lab_External/Galaxea_Lab_External/tasks/direct/galaxea_lab_agent/galaxea_lab_agent_env.py
+++ source/Galaxea_Lab_External/Galaxea_Lab_External/tasks/direct/galaxea_lab_agent/galaxea_lab_agent_env.py
@@ -31,7 +31,7 @@ import isaaclab.envs.mdp as mdp
 
 import isaacsim.core.utils.torch as torch_utils
 
-from Galaxea_Lab_External.robots import GalaxeaRulePolicy
+# Rule-policy class is read from cfg (see ACTIVE_ROBOT_BUNDLE).
 from isaaclab.sensors import Camera
 
 import h5py
@@ -508,7 +508,7 @@ class GalaxeaLabAgentEnv(DirectRLEnv):
             env_ids = self.robot._ALL_INDICES
         super()._reset_idx(env_ids)
 
-        self.rule_policy = GalaxeaRulePolicy(sim_utils.SimulationContext.instance(), self.scene, self.obj_dict)
+        self.rule_policy = self.cfg.rule_policy_class(sim_utils.SimulationContext.instance(), self.scene, self.obj_dict)
         self.initial_root_state = None
 
         self.env_step_action = None
